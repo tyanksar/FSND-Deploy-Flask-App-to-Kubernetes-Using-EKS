cf75a70b3a1f5d002ff23a6295a59ea09ebf306b
tyanksar <63454707+tyanksar@users.noreply.github.com>
2020-07-20 05:29:26 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -14,6 +14,9 @@ PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
+    """
+    client fixture
+    """
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
@@ -31,7 +34,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
